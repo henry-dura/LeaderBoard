831b09cb8a17a53289e4df87cb17ec902bcdbe5e
henry-dura <henryikemefuna@gmail.com>
2023-01-31 22:58:59 +0100

    Fix linter errors

--- src/modules/addNewScore.js
+++ src/modules/addNewScore.js
@@ -1,29 +1,27 @@
-import displayInHtml from "./displayInHtml";
+import displayInHtml from './displayInHtml.js';
 
 const scoreName = document.querySelector('#name');
 const score = document.querySelector('#score');
-export const form = document.querySelector('form')
+export const form = document.querySelector('form');
 
-class CreateScore{
-    constructor(yourName,yourScore){
-        this.yourName = yourName;
-        this.yourScore = yourScore;
-    }
+class CreateScore {
+  constructor(yourName, yourScore) {
+    this.yourName = yourName;
+    this.yourScore = yourScore;
+  }
 
-    static clear(){
-        scoreName.value ='';
-        score.value = '';
-    }
+  static clear() {
+    scoreName.value = '';
+    score.value = '';
+  }
 }
 
-
-const addNewScore = ()=>{
-    if(scoreName.value && score.value >= 0 ){
-        const newScore = new CreateScore(scoreName.value,score.value);
-        displayInHtml(newScore);
-        CreateScore.clear();
-    }
-    
-}
+const addNewScore = () => {
+  if (scoreName.value && score.value >= 0) {
+    const newScore = new CreateScore(scoreName.value, score.value);
+    displayInHtml(newScore);
+    CreateScore.clear();
+  }
+};
 
 export default addNewScore;

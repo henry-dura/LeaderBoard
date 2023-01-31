e040d30474efd0d8ea4daa2ffd1779a13e34e20b
henry-dura <henryikemefuna@gmail.com>
2023-01-31 21:56:10 +0100

    function to display score in Html

---
+++ src/modules/displayInHtml.js
@@ -0,0 +1,9 @@
+const table = document.querySelector('.scores-table');
+
+const displayInHtml = (newScore) => {
+    const tableRow = document.createElement('tr');
+    tableRow.innerHTML = `
+    <td>${newScore.yourName} : ${newScore.yourScore}</td>`
+
+    table.appendChild(tableRow);
+}

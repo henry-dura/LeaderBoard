aae334f4b0952acdbb4a6eaeda6025aaf33a3dde
henry-dura <henryikemefuna@gmail.com>
2023-01-31 21:43:27 +0100

    function to add new score

--- src/modules/newScore.js
+++ src/modules/newScore.js
@@ -1,6 +1,15 @@
+const scoreName = document.querySelector('#name');
+const score = document.querySelector('#score');
+
 class CreateScore{
     constructor(yourName,yourScore){
         this.yourName = yourName;
         this.yourScore = yourScore;
     }
+}
+
+
+const addNewScore = ()=>{
+    const newScore = new CreateScore(scoreName.value,score.value);
+    displayInHtml(newScore);
 }

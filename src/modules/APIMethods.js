39e27c037fdbea9e626308bcc1981d38bf1b33f4
henry-dura <henryikemefuna@gmail.com>
2023-02-02 23:20:56 +0100

    Add display message when Submit button is clicked

--- src/modules/APIMethods.js
+++ src/modules/APIMethods.js
@@ -7,16 +7,37 @@ const getData = async () => {
 };
 
 const postData = async (newScore) => {
-  await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify({
-      user: newScore.user,
-      score: newScore.score,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
+    try{
+        const response =  await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+              user: newScore.user,
+              score: newScore.score,
+            }),
+            headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+            },
+          });
+          if(!response.ok) throw new Error("Score not Submited, Kindly try again");
+          mssg();
+
+    }catch(error){
+        mssg(error)
+    }
+ 
 };
+const success = document.querySelector('.success');
+const failure = document.querySelector('.failure');
+const mssg = (message = 'success') =>{
+    if (message === 'success') {
+        success.classList.remove('hide')
+        setTimeout(()=>{ success.classList.add('hide')},2000);
+       
+    }else{
+        failure.classList.remove('hide')
+        setTimeout(()=>{ failure.classList.add('hide')},2000);
+    }
+    
+}
 
 export { getData, postData };

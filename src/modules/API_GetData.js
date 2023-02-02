20f24dccf3f8187183f26addfae0bd6569d3a6a2
henry-dura <henryikemefuna@gmail.com>
2023-02-02 23:48:27 +0100

    fix linters

--- src/modules/API_GetData.js
+++ src/modules/API_GetData.js
@@ -1,12 +1,9 @@
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/SjEdYh0yO5VuWG6dws4E/scores/';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/YwLfjpGJLs9T4f3VMCZd/scores/';
 
 const getData = async () => {
   const response = await fetch(url);
   const data = await response.json();
   return data.result;
 };
-
-
-
 
 export { getData, url };
